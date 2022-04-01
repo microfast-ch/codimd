c07ffeefc2f3ad38abf739bfc6d8524e8995ddc1
Lukas Läderach <lukas.laederach@microfast.ch>
2022-04-01 15:55:03 +0200

    feat(docker): create docker images

--- deployments/build.sh
+++ deployments/build.sh
@@ -3,10 +3,8 @@
 set -eo pipefail
 set -x
 
-if [[ -z $1 || -z $2 ]];then
-    echo "build.sh [runtime image] [buildpack image]"
-    exit 1
-fi
+RUNTIME_IMAGE=${1:-"12-alpine"}
+BUILD_IMAGE=${2:-"12-bullseye"}
 
 CURRENT_DIR=$(dirname "$BASH_SOURCE")
 
@@ -15,7 +13,4 @@ GIT_SHORT_ID="${GIT_SHA1:0:8}"
 GIT_TAG=$(git describe --exact-match --tags $(git log -n1 --pretty='%h') 2>/dev/null || echo "")
 
 DOCKER_TAG="${GIT_TAG:-$GIT_SHORT_ID}"
-
-docker build --build-arg RUNTIME=$1 --build-arg BUILDPACK=$2 -t "hackmdio/hackmd:$DOCKER_TAG" -f "$CURRENT_DIR/Dockerfile" "$CURRENT_DIR/.."
-
-docker build --build-arg RUNTIME=$1 --build-arg BUILDPACK=$2 -t "hackmdio/hackmd:$DOCKER_TAG-cjk" -f "$CURRENT_DIR/Dockerfile" "$CURRENT_DIR/.."
+docker build --build-arg RUNTIME=${RUNTIME_IMAGE} --build-arg BUILDPACK=${BUILD_IMAGE} -t "codimd:$DOCKER_TAG" -f "$CURRENT_DIR/Dockerfile" "$CURRENT_DIR/.."
